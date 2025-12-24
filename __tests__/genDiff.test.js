87225e984846c36be73324e084d72e0ec20906f2
faceontable <faceontable.ro@gmail.com>
2025-12-24 17:36:31 +0300

    Добавил вложенность

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -9,13 +9,19 @@ const __dirname = path.dirname(__filename)
 const fixturesPath = path.resolve(__dirname, '../__fixtures__')
 
 test('should compare JSON files', () => {
-  const actual = genDiff(path.join(fixturesPath, 'json', 'file1.json'), path.join(fixturesPath, 'json', 'file2.json'))
-  const expected = fs.readFileSync(path.join(fixturesPath, 'json', 'expected.txt'), 'utf-8')
+  const actual = genDiff(path.join(fixturesPath, 'json/flat', 'file1.json'), path.join(fixturesPath, 'json/flat', 'file2.json'))
+  const expected = fs.readFileSync(path.join(fixturesPath, 'json/flat', 'expected.txt'), 'utf-8')
   expect(actual).toBe(expected)
 })
 
 test('should compare YAML files', () => {
   const actual = genDiff(path.join(fixturesPath, 'yaml', 'file1.yaml'), path.join(fixturesPath, 'yaml', 'file2.yaml'))
   const expected = fs.readFileSync(path.join(fixturesPath, 'yaml', 'expected.txt'), 'utf-8')
+  expect(actual).toBe(expected)
+})
+
+test('should compare nested structures', () => {
+  const actual = genDiff(path.join(fixturesPath, 'json/nested', 'file1.json'), path.join(fixturesPath, 'json/nested', 'file2.json'))
+  const expected = fs.readFileSync(path.join(fixturesPath, 'json/nested', 'expected.txt'), 'utf-8')
   expect(actual).toBe(expected)
 })
--- src/genDiff.js
+++ src/genDiff.js
@@ -1,12 +1,15 @@
 import _ from 'lodash'
 import parseFileData from './parsers.js'
 
-export default function genDiff(filePath1, filePath2) {
-  const first = parseFileData(filePath1)
-  const second = parseFileData(filePath2)
+const compareObjects = (first, second, level = 1) => {
   const keys = _.sortBy(_.union(_.keys(first), _.keys(second)))
+  const tab = '  '.repeat(level)
+  let result = `{`
 
-  let result = '{'
+  const formatValue = (val, lvl) => {
+    return val != null && typeof val === 'object' ? compareObjects(val, val, lvl) : val
+  }
+
   for (let key of keys) {
     const firstHasKey = Object.hasOwn(first, key)
     const firstVal = first[key]
@@ -14,21 +17,39 @@ export default function genDiff(filePath1, filePath2) {
     const secondVal = second[key]
 
     if (firstHasKey && secondHasKey) {
-      if (firstVal == secondVal) {
-        result += `\n    ${key}: ${secondVal}`
+      if (firstVal != null && typeof firstVal === 'object' && secondVal != null && typeof secondVal === 'object') {
+        result += `\n${tab}  ${key}: ${compareObjects(firstVal, secondVal, level + 2)}`
+      }
+      else if (firstVal != null && typeof firstVal === 'object') {
+        result += `\n${tab}- ${key}: ${formatValue(firstVal, level + 2)}`
+        result += `\n${tab}+ ${key}: ${secondVal}`
+      }
+      else if (secondVal != null && typeof secondVal === 'object') {
+        result += `\n${tab}- ${key}: ${firstVal}`
+        result += `\n${tab}+ ${key}: ${formatValue(secondVal, level + 2)}`
       }
       else {
-        result += `\n  - ${key}: ${firstVal}`
-        result += `\n  + ${key}: ${secondVal}`
+        if (firstVal == secondVal) {
+          result += `\n${tab}  ${key}: ${secondVal}`
+        }
+        else {
+          result += `\n${tab}- ${key}: ${firstVal}`
+          result += `\n${tab}+ ${key}: ${secondVal}`
+        }
       }
     }
     else if (firstHasKey) {
-      result += `\n  - ${key}: ${firstVal}`
+      result += `\n${tab}- ${key}: ${formatValue(firstVal, level + 2)}`
     }
     else {
-      result += `\n  + ${key}: ${secondVal}`
+      result += `\n${tab}+ ${key}: ${formatValue(secondVal, level + 2)}`
     }
   }
-  result += '\n}'
-  return result
+  return result + `\n${'  '.repeat(level - 1)}}`
+}
+
+export default function genDiff(filePath1, filePath2) {
+  const first = parseFileData(filePath1)
+  const second = parseFileData(filePath2)
+  return compareObjects(first, second)
 }
