aeb83ff3b17b33578afddeff6243878125df6337
faceontable <faceontable.ro@gmail.com>
2025-12-24 16:11:56 +0300

    Поправил подключение линтера

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -4,12 +4,12 @@ import path from 'node:path'
 import url from 'node:url'
 import * as fs from 'node:fs'
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const fixturesPath = path.resolve(__dirname, '../__fixtures__');
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const fixturesPath = path.resolve(__dirname, '../__fixtures__')
 
 test('should compare two files', () => {
-    const actual = genDiff(path.join(fixturesPath, 'file1.json'), path.join(fixturesPath, 'file2.json'))
-    const expected = fs.readFileSync(path.join(fixturesPath, 'expected.txt'), 'utf-8')
-    expect(actual).toBe(expected)
+  const actual = genDiff(path.join(fixturesPath, 'file1.json'), path.join(fixturesPath, 'file2.json'))
+  const expected = fs.readFileSync(path.join(fixturesPath, 'expected.txt'), 'utf-8')
+  expect(actual).toBe(expected)
 })
