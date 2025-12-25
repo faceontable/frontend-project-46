872200d43479b2df60e7cc59ce819dbadb7999ad
faceontable <faceontable.ro@gmail.com>
2025-12-25 14:47:22 +0300

    Добавил плоский формат

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -31,3 +31,15 @@ test('should compare nested YAML structures', () => {
   const expected = fs.readFileSync(path.join(fixturesPath, 'yaml/nested', 'expected.txt'), 'utf-8')
   expect(actual).toBe(expected)
 })
+
+test('should compare nested JSON structures in plain format', () => {
+  const actual = genDiff(path.join(fixturesPath, 'json/nested', 'file1.json'), path.join(fixturesPath, 'json/nested', 'file2.json'), 'plain')
+  const expected = fs.readFileSync(path.join(fixturesPath, 'json/nested', 'expected_plain.txt'), 'utf-8')
+  expect(actual).toBe(expected)
+})
+
+test('should compare nested YAML structures in plain format', () => {
+  const actual = genDiff(path.join(fixturesPath, 'yaml/nested', 'file1.yaml'), path.join(fixturesPath, 'yaml/nested', 'file2.yaml'), 'plain')
+  const expected = fs.readFileSync(path.join(fixturesPath, 'yaml/nested', 'expected_plain.txt'), 'utf-8')
+  expect(actual).toBe(expected)
+})
--- src/genDiff.js
+++ src/genDiff.js
@@ -1,55 +1,141 @@
 import _ from 'lodash'
 import parseFileData from './parsers.js'
 
-const compareObjects = (first, second, level = 1) => {
+const isObject = v => v != null && typeof v === 'object'
+
+const compareObjects = (first = {}, second = {}) => {
+  const result = {}
   const keys = _.sortBy(_.union(_.keys(first), _.keys(second)))
+  for (const key of keys) {
+    const a = first[key]
+    const b = second[key]
+    const aIsObj = isObject(a)
+    const bIsObj = isObject(b)
+
+    if (aIsObj && bIsObj) {
+      result[key] = compareObjects(a, b)
+      continue
+    }
+
+    const aHas = Object.hasOwn(first, key)
+    const bHas = Object.hasOwn(second, key)
+
+    if (!aHas && bHas) {
+      result[key] = { after: b }
+      continue
+    }
+    if (aHas && !bHas) {
+      result[key] = { before: a }
+      continue
+    }
+
+    if (_.isEqual(a, b)) {
+      result[key] = a
+    }
+    else {
+      result[key] = { before: a, after: b }
+    }
+  }
+  return result
+}
+
+const formatDiff = (diff, format) => {
+  if (format === 'stylish') {
+    return formatStylish(diff)
+  }
+  else if (format === 'plain') {
+    return formatPlain(diff)
+  }
+}
+
+const formatStylish = (diff, level = 1) => {
   const tab = '  '.repeat(level)
   let result = `{`
 
-  const formatValue = (val, lvl) => {
-    return val != null && typeof val === 'object' ? compareObjects(val, val, lvl) : val
+  const renderValue = (val, lvl) => {
+    if (val == null || typeof val !== 'object') return val
+    const innerTab = '  '.repeat(lvl)
+    let res = `{\n`
+    for (const k of Object.keys(val)) {
+      res += `${innerTab}  ${k}: ${renderValue(val[k], lvl + 2)}\n`
+    }
+    res += `${'  '.repeat(lvl - 1)}}`
+    return res
   }
 
-  for (let key of keys) {
-    const firstHasKey = Object.hasOwn(first, key)
-    const firstVal = first[key]
-    const secondHasKey = Object.hasOwn(second, key)
-    const secondVal = second[key]
+  for (const key of Object.keys(diff)) {
+    const value = diff[key]
+    const hasOldValue = Object.hasOwn(value, 'before') && diff[key].before !== undefined
+    const hasNewValue = Object.hasOwn(value, 'after') && diff[key].after !== undefined
 
-    if (firstHasKey && secondHasKey) {
-      if (firstVal != null && typeof firstVal === 'object' && secondVal != null && typeof secondVal === 'object') {
-        result += `\n${tab}  ${key}: ${compareObjects(firstVal, secondVal, level + 2)}`
-      }
-      else if (firstVal != null && typeof firstVal === 'object') {
-        result += `\n${tab}- ${key}: ${formatValue(firstVal, level + 2)}`
-        result += `\n${tab}+ ${key}: ${secondVal}`
-      }
-      else if (secondVal != null && typeof secondVal === 'object') {
-        result += `\n${tab}- ${key}: ${firstVal}`
-        result += `\n${tab}+ ${key}: ${formatValue(secondVal, level + 2)}`
-      }
-      else {
-        if (firstVal == secondVal) {
-          result += `\n${tab}  ${key}: ${secondVal}`
-        }
-        else {
-          result += `\n${tab}- ${key}: ${firstVal}`
-          result += `\n${tab}+ ${key}: ${secondVal}`
-        }
-      }
+    if (typeof value === 'object' && !hasOldValue && !hasNewValue) {
+      result += `\n${tab}  ${key}: ${formatStylish(value, level + 2)}`
     }
-    else if (firstHasKey) {
-      result += `\n${tab}- ${key}: ${formatValue(firstVal, level + 2)}`
+    else if (hasOldValue && hasNewValue) {
+      const beforeVal = renderValue(value.before, level + 2)
+      const afterVal = renderValue(value.after, level + 2)
+      result += `\n${tab}- ${key}: ${beforeVal}`
+      result += `\n${tab}+ ${key}: ${afterVal}`
+    }
+    else if (hasOldValue) {
+      const beforeVal = renderValue(value.before, level + 2)
+      result += `\n${tab}- ${key}: ${beforeVal}`
+    }
+    else if (hasNewValue) {
+      const afterVal = renderValue(value.after, level + 2)
+      result += `\n${tab}+ ${key}: ${afterVal}`
     }
     else {
-      result += `\n${tab}+ ${key}: ${formatValue(secondVal, level + 2)}`
+      const plain = renderValue(value, level + 2)
+      result += `\n${tab}  ${key}: ${plain}`
     }
   }
   return result + `\n${'  '.repeat(level - 1)}}`
 }
 
-export default function genDiff(filePath1, filePath2) {
+const formatPlain = (diff) => {
+  const lines = []
+
+  const renderValue = (val) => {
+    if (val == null) return 'null'
+    if (typeof val === 'object') return '[complex value]'
+    if (typeof val === 'string') return `'${val}'`
+    return val
+  }
+
+  const iter = (currentDiff, path) => {
+    for (const key of Object.keys(currentDiff)) {
+      const value = currentDiff[key]
+      const propertyPath = path ? `${path}.${key}` : key
+      const hasOldValue = Object.hasOwn(value, 'before') && value.before !== undefined
+      const hasNewValue = Object.hasOwn(value, 'after') && value.after !== undefined
+
+      if (typeof value === 'object' && !hasOldValue && !hasNewValue) {
+        iter(value, propertyPath)
+      }
+      else if (hasOldValue && hasNewValue) {
+        const beforeVal = renderValue(value.before)
+        const afterVal = renderValue(value.after)
+        lines.push(`Property '${propertyPath}' was updated. From ${beforeVal} to ${afterVal}`)
+      }
+      else if (hasOldValue) {
+        const beforeVal = renderValue(value.before)
+        lines.push(`Property '${propertyPath}' was removed`)
+      }
+      else if (hasNewValue) {
+        const afterVal = renderValue(value.after)
+        lines.push(`Property '${propertyPath}' was added with value: ${afterVal}`)
+      }
+    }
+  }
+
+  iter(diff, '')
+  return lines.join('\n')
+}
+
+export default function genDiff(filePath1, filePath2, format = 'stylish') {
   const first = parseFileData(filePath1)
   const second = parseFileData(filePath2)
-  return compareObjects(first, second)
+  const diff = compareObjects(first, second)
+  return formatDiff(diff, format)
 }
